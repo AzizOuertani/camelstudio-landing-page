036efaa97a143dbf65b5c4a33ed7eea6a365aaff
AzizOuertani <aziz@bearstudio.fr>
2024-01-15 12:13:59 +0100

    Fix : update fonts name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    fontFamily: {
-      thrashand: ["var(--font-thrashand)"],
-      roboto_slab: ["var(--font-roboto)"],
+      fontFamily: {
+        thrashand: ["var(--font-thrashand)"],
+        roboto: ["var(--font-roboto)"],
+      },
     },
   },
   plugins: [],
